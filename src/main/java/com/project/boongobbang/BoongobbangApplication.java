a13ca96bf6a7c3013828bec588afc101e0e39aaa
sanghyuk2 <givemesomefoodplz@gmail.com>
2023-08-23 19:40:16 +0900

    [Setting] Spring Boot 개발 환경 설정

---
+++ src/main/java/com/project/boongobbang/BoongobbangApplication.java
@@ -0,0 +1,13 @@
+package com.project.boongobbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoongobbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoongobbangApplication.class, args);
+	}
+
+}
