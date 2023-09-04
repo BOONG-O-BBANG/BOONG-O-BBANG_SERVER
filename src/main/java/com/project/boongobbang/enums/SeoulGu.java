1ce3bc0930f61151644366942bf4c28ae26ea912
sanghyuk2 <givemesomefoodplz@gmail.com>
2023-09-04 15:23:26 +0900

    [Feat] entity enum 생성

---
+++ src/main/java/com/project/boongobbang/enums/SeoulGu.java
@@ -0,0 +1,21 @@
+package com.project.boongobbang.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum SeoulGu {
+    은평구,
+    종로구,
+    중구,
+    강북구,
+    강서구,
+    광진구,
+    구로구,
+    마포구,
+    관악구,
+    서초구,
+    강남구,
+    강동구,
+    노원구,
+    용산구
+}
