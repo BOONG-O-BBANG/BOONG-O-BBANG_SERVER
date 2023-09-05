abff171e5d9da6b5919c96ae513a715f668f9214
sanghyuk2 <41242710+sanghyuk2@users.noreply.github.com>
2023-09-05 20:44:16 +0900

    [Feature] 토큰 재발행 기능 (#34)
    * [Feat] ReIssueDto 구현 (#33)
    
    * [Feat] reissue api 기능 구현 (#33)

---
+++ src/main/java/com/project/boongobbang/domain/dto/user/ReIssueDto.java
@@ -0,0 +1,10 @@
+package com.project.boongobbang.domain.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReIssueDto {
+    private String userNaverId;
+}
