d317b01e48ff0180ff9589195e853a4e1559fa6f
sanghyuk2 <givemesomefoodplz@gmail.com>
2023-09-05 20:15:44 +0900

    [Feat] ErrorCode 내용 추가

--- src/main/java/com/project/boongobbang/exception/ErrorCode.java
+++ src/main/java/com/project/boongobbang/exception/ErrorCode.java
@@ -9,9 +9,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     USER_ALREADY_EXISTS(HttpStatus.CONFLICT, ""),
     USER_ID_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
     INVALID_CLEAN_COUNT(HttpStatus.NOT_FOUND, ""),
     INVALID_GENDER(HttpStatus.NOT_FOUND, ""),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "");
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, ""),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "");
 
     private final HttpStatus httpStatus;
     private final String message;
