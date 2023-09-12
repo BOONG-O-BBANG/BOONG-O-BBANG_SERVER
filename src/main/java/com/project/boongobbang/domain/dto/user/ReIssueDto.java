1e0f42c8ea918be5472933e161494c1038aae2cc
chu9741 <chu9741@gmail.com>
2023-09-12 12:21:19 +0900

    [Refactor] ReIssueDto 생성자

--- src/main/java/com/project/boongobbang/domain/dto/user/ReIssueDto.java
+++ src/main/java/com/project/boongobbang/domain/dto/user/ReIssueDto.java
@@ -2,9 +2,11 @@ package com.project.boongobbang.domain.dto.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ReIssueDto {
     private String userNaverId;
 }
