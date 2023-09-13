d75829791c57ce27071d22d950feaa3a6bc578f6
sanghyuk2 <givemesomefoodplz@gmail.com>
2023-09-13 15:03:54 +0900

    [BUG] AWS CodeDeploy AllowTraffic 이벤트에서 무한 로딩 문제

--- src/main/java/com/project/boongobbang/config/SecurityConfiguration.java
+++ src/main/java/com/project/boongobbang/config/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration {
                         "/v2/api-docs", "/swagger-resources/**", "/swagger-ui/index.html", "/swagger-ui.html","/webjars/**", "/swagger/**",   // swagger
                         "/h2-console/**",
                         "/favicon.ico","/**/reissue/**",
-                        "/users/signin", "/users/signup", "/users/health").permitAll()
+                        "/users/signin", "/users/signup").permitAll()
                 .antMatchers("/**/admins/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
