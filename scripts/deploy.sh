257578461fa4a8aa914edc86b66a1c1450e30b49
Eungi Jeong <eungi9640@gmail.com>
2023-09-17 00:12:28 +0900

    [BUG] Docker 미사용 이미지 있는 경우 삭제

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,8 +23,9 @@ sudo docker pull "$image_name"
 # 컨테이너 실행
 docker run -d -p 80:8080 --name "$container_name" "$image_name"
 
-# 미사용 이미지 삭제
-docker rmi -f $(docker images -f "dangling=true" -q) || true
-
+# 미사용 이미지 있는 경우 삭제
+if [ "$(docker images -f "dangling=true" -q)" != "" ]; then
+    docker rmi -f $(docker images -f "dangling=true" -q)
+fi
 # Redis 설치
 sudo apt-get install -y redis-server
