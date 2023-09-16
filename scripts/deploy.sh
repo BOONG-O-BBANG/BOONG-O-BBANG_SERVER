2e458570a2e77248028676433d061c0e03c9df67
sanghyuk2 <41242710+sanghyuk2@users.noreply.github.com>
2023-09-16 15:08:26 +0900

    [Deploy] deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,13 +6,19 @@ image_name="givemesomefoodplz/boong-o-bbang_server:1.0"
 # 컨테이너 이름
 container_name="boong"
 
+# 이미지 확인
+if sudo docker images | grep -q "$image_name"; then
+    # 이미지가 존재하면 삭제
+    sudo docker rmi "$image_name"
+fi
+
 # 컨테이너 확인 및 중지/제거
-if docker ps -a -q --filter "name=$container_name" | grep -q .; then
-    docker stop "$container_name" && docker rm "$container_name"
+if sudo docker ps -a -q --filter "name=$container_name" | grep -q .; then
+    sudo docker stop "$container_name" && sudo docker rm "$container_name"
 fi
 
 # 이미지 다운로드
-docker pull "$image_name"
+sudo docker pull "$image_name"
 
 # 컨테이너 실행
 docker run -d -p 80:8080 --name "$container_name" "$image_name"
