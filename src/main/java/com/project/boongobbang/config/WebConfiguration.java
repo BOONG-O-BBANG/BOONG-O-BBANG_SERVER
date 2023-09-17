70b6be2296c86f0cfd9683ebf44f3fc5ad650275
Eungi Jeong <eungi9640@gmail.com>
2023-09-17 21:39:03 +0900

    [BUG] CORS 설정에서 발생하는 문제 (#117)

--- src/main/java/com/project/boongobbang/config/WebConfiguration.java
+++ src/main/java/com/project/boongobbang/config/WebConfiguration.java
@@ -12,10 +12,9 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS") // 허용할 HTTP 메소드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowedOrigins(
-                        "http://boong-vpc-ec2-deploy-lb-999176414.ap-northeast-2.elb.amazonaws.com/swagger-ui.html#/",
+                        "http://boong-vpc-ec2-deploy-lb-999176414.ap-northeast-2.elb.amazonaws.com",
                         "http://d2bczezs33iv0e.cloudfront.net"
                 ) // 허용할 origin을 명시적으로 설정
-
                 .allowCredentials(true); // 쿠키, 헤더 정보 등을 포함할 지 결정
     }
 }
