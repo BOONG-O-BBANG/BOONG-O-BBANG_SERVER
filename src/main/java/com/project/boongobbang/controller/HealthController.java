d3d50e1bef31a70867e2532d626eec91f14573e9
eunki96 <eungi9640@gmail.com>
2023-09-18 18:02:49 +0900

    [Docs] CI/CD 테스트

--- src/main/java/com/project/boongobbang/controller/HealthController.java
+++ src/main/java/com/project/boongobbang/controller/HealthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class HealthController {
-    @ApiOperation("헬스 체크")
+    @ApiOperation("Health Check")
     @ApiResponses(value={
             @ApiResponse(code = 200,
                     message = "HEALTH_CHECKED",
